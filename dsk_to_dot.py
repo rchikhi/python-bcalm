56c15c15cfbde05e99e89659717fb8f031f62bec
Avi Srivastava <k3yavi@users.noreply.github.com>
2016-12-01 22:38:28 -0500

    Update dsk_to_dot.py

--- dsk_to_dot.py
+++ dsk_to_dot.py
@@ -1,6 +1,8 @@
 import sys
 with open(sys.argv[1]+".dot",'w') as r:
     with open(sys.argv[1]) as f:
+        r.write("digraph G { \n")
         for line in f:
             current = line.strip().lower().split()[0]
             r.write("%s;\n" % current)
+        r.write("}")
